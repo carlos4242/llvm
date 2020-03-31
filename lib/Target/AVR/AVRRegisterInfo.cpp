235f9f81a5b624ca1dac99a97dca81925529ede4
Dylan McKay <me@dylanmckay.io>
2020-03-31 19:00:18 +1300

    Respect the 'interrupt' function attribute
    In the past, AVR functions were only lowered with interrupt-specific
    machine code if the function was defined with the "avr-interrupt" or
    "avr-signal" calling conventions.
    
    This patch modifies the backend so that if the function does not have a
    special calling convention, but does have an "interrupt" attribute,
    that function is interpreted as a function with interrupts.
    
    This also extracts the "is this function an interrupt" logic from
    several disparate places in the backend into one AVRMachineFunctionInfo
    attribute.
    
    Bug found by Wilhelm Meier.

--- lib/Target/AVR/AVRRegisterInfo.cpp
+++ lib/Target/AVR/AVRRegisterInfo.cpp
@@ -23,6 +23,7 @@
 
 #include "AVR.h"
 #include "AVRInstrInfo.h"
+#include "AVRMachineFunctionInfo.h"
 #include "AVRTargetMachine.h"
 #include "MCTargetDesc/AVRMCTargetDesc.h"
 
@@ -35,19 +36,21 @@ AVRRegisterInfo::AVRRegisterInfo() : AVRGenRegisterInfo(0) {}
 
 const uint16_t *
 AVRRegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const {
-  CallingConv::ID CC = MF->getFunction().getCallingConv();
+  const AVRMachineFunctionInfo *AFI = MF->getInfo<AVRMachineFunctionInfo>();
 
-  return ((CC == CallingConv::AVR_INTR || CC == CallingConv::AVR_SIGNAL)
+  return AFI->isInterruptHandler()
               ? CSR_Interrupts_SaveList
-              : CSR_Normal_SaveList);
+              : CSR_Normal_SaveList;
 }
 
 const uint32_t *
 AVRRegisterInfo::getCallPreservedMask(const MachineFunction &MF,
                                       CallingConv::ID CC) const {
-  return ((CC == CallingConv::AVR_INTR || CC == CallingConv::AVR_SIGNAL)
+  const AVRMachineFunctionInfo *AFI = MF.getInfo<AVRMachineFunctionInfo>();
+
+  return AFI->isInterruptHandler()
               ? CSR_Interrupts_RegMask
-              : CSR_Normal_RegMask);
+              : CSR_Normal_RegMask;
 }
 
 BitVector AVRRegisterInfo::getReservedRegs(const MachineFunction &MF) const {
